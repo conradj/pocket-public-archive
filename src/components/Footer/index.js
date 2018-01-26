f2f2be3faff97dd1cac175b893ee4bb0a3cea444
conradj <conradsjackson@gmail.com>
2018-01-26 22:34:45 +0000

    add footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "gatsby-link";
+
+const Footer = () => (
+  <div
+    style={{
+      background: "red",
+      paddingBottom: "1.45rem"
+    }}
+  >
+    <div
+      style={{
+        margin: "0 auto",
+        maxWidth: 960,
+        padding: "1.45rem 1.0875rem"
+      }}
+    >
+      <h4 style={{ margin: 0, float: "left" }}>
+        <a
+          href="https://conradj.co.uk"
+          style={{
+            color: "white"
+          }}
+        >
+          Written by conradj.co.uk
+        </a>
+      </h4>
+    </div>
+  </div>
+);
+
+export default Footer;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import "./index.css";
 
 const TemplateWrapper = ({ children }) => (
@@ -25,6 +26,7 @@ const TemplateWrapper = ({ children }) => (
     >
       {children()}
     </div>
+    <Footer />
   </div>
 );
 
