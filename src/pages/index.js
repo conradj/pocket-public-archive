9c548758c56edac343b3ac2104b7eb4fc77f9ee8
conradj <conradsjackson@gmail.com>
2018-02-08 22:21:39 +0000

    attempt the prefix paths again

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
-
+import { withPrefix } from "gatsby-link";
 const startOfWeek = require("date-fns/start_of_week");
 const format = require("date-fns/format");
 
 const IndexPage = pluginOptions => {
-  const thisWeekTime = parseInt(format(startOfWeek(new Date()), "X"));
+  const thisWeekTime = withPrefix(
+    parseInt(format(startOfWeek(new Date()), "X"))
+  );
   console.log(thisWeekTime);
   //window.location = thisWeekTime;
 
