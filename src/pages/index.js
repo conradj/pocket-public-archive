f8691e5ecf136e50ae56a90ced18250c8fe9422b
conradj <conradsjackson@gmail.com>
2018-02-09 07:06:23 +0000

    turn of redirect :-/

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,9 @@ const IndexPage = pluginOptions => {
   return (
     <div>
       <Helmet>
-        <meta http-equiv="refresh" content={`0;url=${thisWeekTime}`} />;
+        {/* <meta http-equiv="refresh" content={`0;url=${thisWeekTime}`} />; */}
       </Helmet>
+      meta http-equiv="refresh" content={`0;url=${thisWeekTime}`} /;
       <a href={thisWeekTime}>This weeks articles</a>
     </div>
   );
