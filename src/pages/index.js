a1d1771e83d7378483a8ea57dd358dd01c15281a
conradj <conradsjackson@gmail.com>
2018-03-20 22:12:21 +0000

    simplify redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,22 +6,12 @@ const startOfWeek = require("date-fns/start_of_week");
 import format from "date-fns/format";
 
 const IndexPage = pluginOptions => {
-  const today = new Date();
-  let lastWeek = new Date();
-  lastWeek.setDate(today.getDate() - 7);
-  console.log("lastweek", lastWeek);
-  const lastWeekTime = withPrefix(
-    `/${parseInt(format(startOfWeek(lastWeek), "X"))}`
-  );
-  console.log(lastWeekTime);
-  //window.location = thisWeekTime;
-
   return (
     <div className="page-main-container">
       <Helmet>
-        <meta http-equiv="refresh" content={`0;url=${lastWeekTime}`} />;
+        <meta http-equiv="refresh" content={`0;url=lastweek`} />;
       </Helmet>
-      <a href={lastWeekTime}>Articles read in the last week</a>
+      <a href="lastweek">Articles read in the last week</a>
     </div>
   );
 };
