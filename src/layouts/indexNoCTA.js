de70fade2f909fb4c71d5ae7d51ee78e5058429c
conradj <conradsjackson@gmail.com>
2019-05-04 09:35:37 +0100

    remove cta from social card

---
+++ src/layouts/indexNoCTA.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import fontawesome from "@fortawesome/fontawesome";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "./index.css";
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet
+      title="Articles I've read"
+      meta={[
+        { name: "description", content: "Sample" },
+        { name: "keywords", content: "sample, something" }
+      ]}
+    >
+      <script
+        defer
+        src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"
+      />
+      <style>{fontawesome.dom.css()}</style>
+    </Helmet>
+    <Header />
+    <div>{children()}</div>
+    <Footer />
+  </div>
+);
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func
+};
+
+export default TemplateWrapper;
